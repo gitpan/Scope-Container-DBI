d9a78c8951495ed77f71a6af14761bb1df95f59d
Masahiro Nagano <kazeburo@gmail.com>
2013-04-08 02:50:18 +0000

    Import of KAZEBURO/Scope-Container-DBI-0.09 from CPAN.
    gitpan-cpan-distribution: Scope-Container-DBI
    gitpan-cpan-version:      0.09
    gitpan-cpan-path:         KAZEBURO/Scope-Container-DBI-0.09.tar.gz
    gitpan-cpan-author:       KAZEBURO
    gitpan-cpan-maturity:     released

--- lib/Scope/Container/DBI.pm
+++ lib/Scope/Container/DBI.pm
@@ -12,7 +12,7 @@ use Module::Load qw/load/;
 use Carp;
 use DBI 1.615;
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 our $DBI_CLASS = 'DBI';
 
 sub connect {
@@ -95,6 +95,7 @@ sub _build_dsn_key {
     my @dsn = @_;
     local $Data::Dumper::Terse = 1;
     local $Data::Dumper::Indent = 0;
+    local $Data::Dumper::Sortkeys = 1;
     my $key = Data::Dumper::Dumper(\@dsn);
     "sc:dbix:".$key;
 }
